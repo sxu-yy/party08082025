57677cebe42341473dda5c323dd4e0a0cf0fd359
sxu-yy <52720183+sxu-yy@users.noreply.github.com>
2025-08-03 02:12:39 -0500

    Update mailbox.php

--- mailbox.php
+++ mailbox.php
@@ -1,5 +1,13 @@
 <?php
-  $mail = htmlspecialchars($_POST["tosend"]);
+  $mail = $_POST['tosend'];
+  $fname = "mailbox.txt";
+  $fh = fopen($fname, 'a') or die("Can't open file");
+  fwrite($fh, $mail);
+  fwrite($fh, "\n");
+  fclose($fh);
 
-  echo $mail;
+  $fh = fopen($fname, 'r');
+  $contents = fread($fh, filesize($fname));
+  fclose($fh);
+  echo $contents;
 ?>
